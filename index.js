d409d5e9b41522fd07466a1e633c0096ee8a1d40
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-09-02 16:30:36 -0400

    decent first page

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+
+app.set('views', './views')
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+app.use(express.static('./static'));
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ' + app.get('port') + '!');
+});
+
