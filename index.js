4a41fbc3ca04b9820c0cfb1deabba4d66b84fe9a
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-09-03 16:25:49 -0400

    working contact page

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
+app.set('email_host', process.env.EMAIL_HOST);
+app.set('email_from', process.env.EMAIL_FROM);
+app.set('email_pass', process.env.EMAIL_PASS);
+app.set('email_to', process.env.EMAIL_TO);
+
+if (app.get('email_host') && app.get('email_from') && app.get('email_pass')){
+  var smtpConfig = {
+    host: app.get('email_host'),
+    port: 465,
+    secure: true,
+    auth: {
+      user: app.get('email_from'),
+      pass: app.get('email_pass')
+    }
+  };
+  var transporter = nodemailer.createTransport(smtpConfig);
+} else {
+  console.log("Warning: Email not configured");
+}
 
 app.set('port', (process.env.PORT || 8080));
 
@@ -22,9 +44,75 @@ app.get('/contact', function (req, res) {
   res.render('contact');
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/contact', function (req, res) {
+  console.log('Message send request:');
+  console.log(req.body);
+  sendContact(req.body, function (results){
+    res.render('contact', {error: results.error, message: results.message, orig: req.body});
+  });
+});
+
 app.use(express.static('./static'));
 
 app.listen(app.get('port'), function () {
-  console.log('Example app listening on port ' + app.get('port') + '!');
+  console.log('Listening on port ' + app.get('port') + '!');
 });
 
+function sendContact(obj, cb){
+  if (!transporter || !app.get('email_to')){
+    console.log("Error: Email not configured");
+    cb({error: true, message: "Email not configured."});
+    return;
+  }
+  var items = [obj.name, obj.email, obj.subject, obj.message];
+  var fns = [validName, validEmail, validSubject, validMessage];
+  var messages = ["Invalid name.", "Invalid email.", "Invalid subject.", "Invalid message."];
+  for (var i = 0; i < items.length; i++){
+    if (!fns[i](items[i])){
+      console.log("Error: " + messages[i]);
+      cb({error: true, message: messages[i]});
+      return;
+    }
+  }
+  
+  var mailOptions = {
+    from: obj.name + ' <' + app.get('email_from') + '>',
+    replyTo: obj.name + ' <' + obj.email + '>',
+    to: app.get('email_to'),
+    subject: "Contact: " + obj.subject,
+    text: "From: " + obj.name + ' <' + obj.email + '>\n\n' + obj.message
+  };
+  console.log('Sending message:');
+  console.log(mailOptions);
+  
+  transporter.sendMail(mailOptions, function (error, info){
+    if (error){
+      console.log('Message failed to send:');
+      console.log(error);
+      cb({error: true, message: String(error)});
+      return;
+    }
+    console.log('Message sent:');
+    console.log(info);
+    cb({message: "Message sent sucessfully!"});
+  });
+}
+
+function validName(a){
+  return /^[^<>]+$/.test(a);
+}
+
+function validEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validSubject(a){
+  return /^.+$/.test(a);
+}
+
+function validMessage(a){
+  return /^.+$/.test(a);
+}
