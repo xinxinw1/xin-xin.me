ea11b352ad3ca3756c73498d46277d9b97013f87
Xin-Xin Wang - blueberry <get@xin-xin.me>
2018-09-12 15:00:14 -0400

    add code blocks

--- index.js
+++ index.js
@@ -3,6 +3,19 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var hljs = require('highlight.js');
+var md = require('markdown-it')({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+               hljs.highlight(lang, str, true).value +
+               '</code></pre>';
+      } catch (__) {}
+    }
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+});
 var compositions = require("./compositions.json");
 var sendContact = require('./contact.js');
 
@@ -12,6 +25,12 @@ app.set('port', process.env.PORT || 8080);
 app.set('hostname', process.env.HOSTNAME || undefined);
 app.set('views', __dirname + '/views')
 app.set('view engine', 'pug');
+
+app.locals.filters = {
+  'markdown': function (text, options) {
+    return md.render(text, options);
+  }
+};
 
 /* Route for index */
 
