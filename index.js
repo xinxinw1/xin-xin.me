2c0f9ce1993e0d0adf1c6b398f8b79831698dda8
Xin-Xin Wang - blueberry <get@xin-xin.me>
2018-09-12 16:23:32 -0400

    support inline code

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var md = require('markdown-it')({
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return '<pre class="hljs"><code>' +
+        return '<pre><code class="hljs">' +
                hljs.highlight(lang, str, true).value +
                '</code></pre>';
       } catch (__) {}
     }
-    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    return '<pre><code class="hljs">' + md.utils.escapeHtml(str) + '</code></pre>';
   }
 });
 var compositions = require("./compositions.json");
